79cf19157bc19a3313cd0dd0fbd488c0f14dfa82
Matt Badger <matt@lighthouseuk.net>
2016-07-18 18:15:58 +0100

    Version bump.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name='jerboa',
     packages=['jerboa'],  # this must be the same as the name above
-    version='0.1.3-alpha',
+    version='0.1.4-alpha',
     description='',
     author='Matt Badger',
     author_email='foss@lighthouseuk.net',
     url='https://github.com/LighthouseUK/jerboa',  # use the URL to the github repo
-    download_url='https://github.com/LighthouseUK/jerboa/tarball/0.1.3-alpha',  # I'll explain this in a second
+    download_url='https://github.com/LighthouseUK/jerboa/tarball/0.1.4-alpha',  # I'll explain this in a second
     keywords=['gae', 'lighthouse', 'jerboa', 'webapp2'],  # arbitrary keywords
     classifiers=[],
     requires=['webapp2', 'blinker', 'inflection', 'wtforms', 'webob', 'jinja2', 'pytz', 'babel'],
